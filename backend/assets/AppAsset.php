f4e25f2e677bad5983b4fa70abbd9a578ee14a31
rickyarmnd <rickyarmnd@gmail.com>
2023-04-02 22:23:10 +0700

    install sb admin template

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,19 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        // 'css/site.css',
+        'vendor/fontawesome-free/css/all.min.css',
+        'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
+        'css/sb-admin-2.min.css'
+
     ];
     public $js = [
+        "vendor/jquery/jquery.min.js",
+        "vendor/jquery-easing/jquery.easing.min.js",
+        "js/sb-admin-2.min.js",
+        "vendor/chart.js/Chart.min.js",
+        "js/demo/chart-area-demo.js",
+        "s/demo/chart-pie-demo.js",
     ];
     public $depends = [
         'yii\web\YiiAsset',
